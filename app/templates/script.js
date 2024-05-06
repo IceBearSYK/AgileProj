b99f6981b29d8da298bc842f91842e9c5641614f
Imrea1m <23102103@student.uwa.edu.au>
2024-05-06 18:27:29 +0800

    quick update

--- app/templates/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('signupButton').addEventListener('button', function(event) {
-    // Prevent the default form submission behavior
-    
-    // Redirect to the signup page
-    location.href = '/signup';
-});
