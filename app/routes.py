cb5b453c5b913ddb8df5adc14a932b941aed7a1b
FonkayPonkay <abdialime30@gmail.comm>
2024-05-19 02:14:32 +0800

    fixed the some issue with login and session

--- app/routes.py
+++ app/routes.py
@@ -15,11 +15,17 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and check_password_hash(user.password, password):
             session['user'] = user.usrID
-            return render_template('HomePage.html')
+            return redirect(url_for('home'))
         else:
             flash("Invalid username or password")
-            return render_template('loginpage.html')
+            return redirect(url_for('login'))
     return render_template('loginpage.html')
+def logout():
+    print("You have been logged out: session = {session}")
+    session.pop("user")
+    flash("You have been logged out")
+    print(" logout successful")
+    return redirect(url_for('login'))
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -31,22 +37,22 @@ def signup():
         
         if password != confirmpsw:
             flash("Passwords do not match")
-            return render_template('signup.html')
+            return redirect(url_for('signup'))
         
         if User.query.filter_by(username=username).first():
             flash("Username already exists")
-            return render_template('signup.html')
+            return redirect(url_for('signup'))
         
         if User.query.filter_by(email=email).first():
             flash("Email already exists")
-            return render_template('signup.html')
+            return redirect(url_for('signup'))
         
         passwordhash = generate_password_hash(password)
         newuser = User(username=username, email=email, password=passwordhash)
         db.session.add(newuser)
         db.session.commit()
         flash("Successfully signed up. Thank you!")
-        return render_template('loginpage.html')
+        return redirect(url_for('login'))
     return render_template('signup.html')
     
 @app.route("/HomePage")
@@ -56,3 +62,6 @@ def MainPage():
 @app.route("/Forums")
 def Forums():
     return render_template('forum.html')
+@app.route("/sesh")
+def checksesh():
+    return f"session right now = {session}"
