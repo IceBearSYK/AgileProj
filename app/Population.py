1caa9b896ceff344ab1c16d10c7c47d5db8b8323
AfricanAlien <dirk.haasbroek7@gmail.com>
2024-05-19 19:21:56 +0800

    fixed popullation.py returning null

--- app/Population.py
+++ app/Population.py
@@ -24,12 +24,15 @@ with app.app_context():
     chat1 = Chat(topic='topic1', username=user1.usrID)
     chat2 = Chat(topic='topic2', username=user2.usrID)
 
+    db.session.add(chat1)
+    db.session.add(chat2)
+    db.session.commit()
+
     message1 = Message(content='content1', chat_id=chat1.id, user_id=user1.usrID)
     message2 = Message(content='content2', chat_id=chat2.id, user_id=user2.usrID)
 
     # Add new entries to the session
-    db.session.add(chat1)
-    db.session.add(chat2)
+    
     db.session.add(message1)
     db.session.add(message2)
 
