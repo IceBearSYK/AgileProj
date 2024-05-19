0c56aa826f9fb2156f963c1932f015dd70344214
AfricanAlien <dirk.haasbroek7@gmail.com>
2024-05-19 13:40:43 +0800

    add Functionality for create new forum

--- app/model.py
+++ app/model.py
@@ -9,11 +9,6 @@ class User(db.Model):
 
 class Chat(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    topic = db.Column(db.String(30), nullable=False)
     username = db.Column(db.String(80), nullable=False)
     message = db.Column(db.String(200), nullable=False)
-
-class ForumPost(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), nullable=False)
-    post = db.Column(db.String(200), nullable=False)
-    username = db.Column(db.String(80), nullable=False)
